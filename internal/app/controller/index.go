f161e60b468e749e26abe1572e23a6c5789ddc29
Jk-97 <1179160244@qq.com>
2018-10-29 16:55:17 +0800

    test api gateway redirect

--- internal/app/controller/index.go
+++ internal/app/controller/index.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 )
 
-// IndexGET displays the home page
-func IndexGET(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadFile("web/template/index.html")
+func serveStatic(path string, w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadFile(path)
 	if err != nil {
 		Error500(w, r)
 		return
 	}
 
 	w.Write(body)
+}
+
+// IndexGET displays the home page
+func IndexGET(w http.ResponseWriter, r *http.Request) {
+	serveStatic("web/template/index.html", w, r)
 }
