b76aa3a6927c7919b6276e42568954bb4cbe36af
Jk-97 <1179160244@qq.com>
2018-10-30 12:08:51 +0800

    Rename: model/message -> model/data-in

--- internal/app/model/data-in.go
+++ internal/app/model/data-in.go
@@ -13,7 +13,7 @@ import (
 func Listen(addr, topic string) {
 	go func() {
 		for {
-			err := listenUntilError(addr, topic)
+			err := listenUntilErr(addr, topic)
 
 			log.Printf("%s", err)
 			time.Sleep(time.Second * 1)
@@ -23,7 +23,7 @@ func Listen(addr, topic string) {
 
 // listen subscribes to messages from redis, and handle the message.
 // Returns error if connection failed.
-func listenUntilError(addr, topic string) error {
+func listenUntilErr(addr, topic string) error {
 	conn, err := redis.Dial("tcp", addr)
 	if err != nil {
 		return err
@@ -35,7 +35,10 @@ func listenUntilError(addr, topic string) error {
 	for {
 		switch v := psc.Receive().(type) {
 		case redis.Message:
-			process(v)
+			err := process(v)
+			if err != nil {
+				log.Printf("%s", err)
+			}
 		case redis.Subscription:
 			fmt.Printf("%s: %s %d\n", v.Channel, v.Kind, v.Count)
 		case error:
