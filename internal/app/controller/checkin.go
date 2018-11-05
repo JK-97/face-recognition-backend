26ab93c69f48f3ad3cc700c59d825b98ffe24517
Jk-97 <1179160244@qq.com>
2018-11-05 12:58:59 +0800

    fix: implement Checkiner struct as single entry point to handle checkin request

--- internal/app/controller/checkin.go
+++ internal/app/controller/checkin.go
@@ -9,13 +9,13 @@ import (
 
 // CheckStatusGET returns checkin status "checking/stopped"
 func CheckStatusGET(w http.ResponseWriter, r *http.Request) {
-	status := checkin.Status()
+	status := checkin.DefaultCheckiner.Status()
 	respondJSON(schema.CheckStatusResp{Status: status}, w, r)
 }
 
 // StartCheckinPOST starts check in
 func StartCheckinPOST(w http.ResponseWriter, r *http.Request) {
-	err := checkin.StartCheckin()
+	err := checkin.DefaultCheckiner.Start()
 	if err != nil {
 		Error(w, err, http.StatusBadRequest)
 		return
@@ -24,7 +24,7 @@ func StartCheckinPOST(w http.ResponseWriter, r *http.Request) {
 
 // StopCheckinPOST stops check in
 func StopCheckinPOST(w http.ResponseWriter, r *http.Request) {
-	t, err := checkin.StopCheckin()
+	t, err := checkin.DefaultCheckiner.Stop()
 	if err != nil {
 		Error(w, err, http.StatusBadRequest)
 		return
