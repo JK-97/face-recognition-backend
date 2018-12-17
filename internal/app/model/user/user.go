71c1da099a859d21d4486aad30cef438d751277f
JK-97 <benjamin-dream@outlook.com>
2018-12-17 16:54:18 +0800

    create default user if user not exist

--- internal/app/model/user/user.go
+++ internal/app/model/user/user.go
@@ -16,8 +16,20 @@ func collection() *mongo.Collection {
 	return model.DB.Collection("user")
 }
 
-// FindUser get cameralist in db
+var userInited = false
+func initUser(){
+	if !userInited {
+		userInited = true
+		num, err := collection().Count(context.Background(), map[string]string{})
+		if err == nil && num == 0 {
+			collection().InsertOne(context.Background(), map[string]string{"username": "admin", "password": "e10adc3949ba59abbe56e057f20f883e", "_id": "admin"})
+		}
+	}
+}
+
+// FindUser get users in db
 func FindUser(userName string) (*schema.User, error) {
+	initUser()
 	doc := collection().FindOne(context.Background(), map[string]string{"username": userName})
 	result := &schema.User{}
 	err := doc.Decode(&result)
