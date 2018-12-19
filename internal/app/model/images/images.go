1cdaf4f824dd79693ce9ec65d357c76e343b71f1
JK-97 <benjamin-dream@outlook.com>
2018-12-19 16:33:43 +0800

    fix bugs: images size bigger than mongo doc limits

--- internal/app/model/images/images.go
+++ internal/app/model/images/images.go
@@ -16,33 +16,56 @@ func collection() *mongo.Collection {
 
 // GetImages return a person full images
 func GetImages(id string) (*schema.DBImages, error){
-	doc := collection().FindOne(context.Background(), map[string]string{"pid": id})
-	result := &schema.DBImages{}
-	err := doc.Decode(&result)
+    ctx := context.Background()
+	cur, err := collection().Find(ctx, map[string]string{"pid": id})
     if err != nil {
         return nil, err
+    }
+	defer cur.Close(ctx)
+
+	result := &schema.DBImages{
+        PID: id,
+    }
+
+	for cur.Next(ctx) {
+		dbp := &schema.DBImages{}
+		if err := cur.Decode(dbp); err != nil {
+			return nil, err
+		}
+        result.Images = append(result.Images, dbp.Image)
     }
     return result, nil
 }
 
 // UpdateImages update a person's images in db
 func UpdateImages(ID string, imgs []string) error {
-    updater := map[string]schema.DBImagesUpdater{"$set": schema.DBImagesUpdater{
-        PID:     ID,
-        Images: imgs,
-    }}
-    _, err := collection().UpdateOne(context.Background(), map[string]string{"pid": ID}, updater)
-    return err
+    for index, img := range imgs {
+        updater := map[string]schema.DBImagesUpdater{"$set": schema.DBImagesUpdater{
+            PID:        ID,
+            Image:      img,
+        }}
+        _, err := collection().UpdateOne(context.Background(), map[string]string{"pid": ID, "image_id": string(index)}, updater)
+        if err != nil {
+            return err
+        }
+    }
+    return nil
 }
 
 // AddImages add a person's image in db
 func AddImages(id string, imgs []string) error {
-	uid, _ := uuid.NewUUID()
-    doc := schema.DBImages{
-        ID:             uid.String(),
-        PID:            id,
-        Images:         imgs,
+    for index, img := range imgs {
+        uid, _ := uuid.NewUUID()
+        doc := schema.DBImages{
+            ID:             uid.String(),
+            PID:            id,
+            Image:          img,
+            ImageID:        string(index),
+        }
+        _, err := collection().InsertOne(context.Background(), doc)
+        if err != nil {
+            return err
+        }
     }
-	_, err := collection().InsertOne(context.Background(), doc)
-    return err
+    return nil
 }
